ba82b8787e81d78873b7ee61aef6df8dbe797310
enjarai <enjarai123@gmail.com>
2023-10-04 00:12:58 +0200

    Initial Commit

---
+++ src/main/java/dev/enjarai/blahajtotem/mixin/BipedEntityModelMixin.java
@@ -0,0 +1,41 @@
+package dev.enjarai.blahajtotem.mixin;
+
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BipedEntityModel.class)
+public class BipedEntityModelMixin {
+
+    @Shadow
+    public @Final ModelPart rightArm;
+
+    @Shadow
+    public @Final ModelPart leftArm;
+
+    @Inject(
+            method = {"positionRightArm", "positionLeftArm"},
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/entity/model/CrossbowPosing;hold(Lnet/minecraft/client/model/ModelPart;Lnet/minecraft/client/model/ModelPart;Lnet/minecraft/client/model/ModelPart;Z)V",
+                    shift = At.Shift.AFTER
+            ),
+            cancellable = true
+    )
+    public void poseArms(LivingEntity entity, CallbackInfo ci) {
+        if(entity.getMainHandStack().isOf(Items.TOTEM_OF_UNDYING) || entity.getOffHandStack().isOf(Items.TOTEM_OF_UNDYING)) {
+            this.rightArm.pitch = -0.95F;
+            this.rightArm.yaw = (float) (-Math.PI / 8);
+            this.leftArm.pitch = -0.90F;
+            this.leftArm.yaw = (float) (Math.PI / 8);
+            ci.cancel();
+        }
+    }
+}
---
+++ src/main/java/dev/enjarai/blahajtotem/mixin/PlayerEntityRendererMixin.java
@@ -0,0 +1,28 @@
+package dev.enjarai.blahajtotem.mixin;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+    @Inject(
+            method = "getArmPose(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;",
+            at = @At("TAIL"),
+            cancellable = true
+    )
+    private static void cuddleBlahaj(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> ci) {
+        ItemStack lv = player.getStackInHand(hand);
+        if(lv.isOf(Items.TOTEM_OF_UNDYING)) {
+            ci.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_HOLD);
+            ci.cancel();
+        }
+    }
+}
