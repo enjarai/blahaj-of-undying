e33319c69707d347980624311f3346836ec26af6
enjarai <enjarai123@gmail.com>
2024-09-12 13:58:04 +0200

    Push :3

--- src/main/java/dev/enjarai/blahajtotem/BlahajFlags.java
+++ src/main/java/dev/enjarai/blahajtotem/BlahajFlags.java
@@ -1,14 +1,15 @@
 package dev.enjarai.blahajtotem;
 
 import dev.enjarai.blahajtotem.pond.BakedHuggableModel;
+import net.fabricmc.fabric.api.renderer.v1.model.WrapperBakedModel;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 public class BlahajFlags {
     public static boolean isHuggable(ItemStack itemStack, LivingEntity entity) {
         var model = MinecraftClient.getInstance().getItemRenderer().getModel(itemStack, entity.getWorld(), entity, 0);
+        model = WrapperBakedModel.unwrap(model);
         return model instanceof BakedHuggableModel huggableModel && huggableModel.blahaj_totem$isHuggable();
     }
 }
